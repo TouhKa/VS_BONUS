ae034e3725a839f7b7a2ed95c9e519b3e1ec6a41
Pia Schmidt <pia.s.schmidt@gmail.com>
2020-11-12 12:59:11 +0100

    Add fibonacci service

---
+++ lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/ws/FibonacciServiceImpl.java
@@ -0,0 +1,24 @@
+package de.hska.iwi.vislab.lab1.example.ws;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "de.hska.iwi.vislab.lab1.example.ws.FibonacciServiceIntf")
+public class FibonacciServiceImpl implements FibonacciServiceIntf {
+    static int n1=0,n2=1,n3=0;
+    //TOOD @Override interface methods
+    @Override
+    public int getFibonacci(@WebParam(name = "n") int n){
+        return calculateNthFibonacci(n);
+    }
+
+    private int calculateNthFibonacci(int n) {
+        System.out.println(n);
+        if(n>0){
+               this.n3 = this.n1 + this.n2;
+               this.n1 = this.n2;
+               this.n2 = this.n3;
+           }
+        return this.n3;
+    }
+}
