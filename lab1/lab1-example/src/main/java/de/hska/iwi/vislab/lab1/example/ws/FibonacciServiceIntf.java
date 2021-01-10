dc5daf57152c0faac70eadc92bd2a12bc2d0459d
Daniel <danielkispert+github@googlemail.com>
2021-01-10 11:11:33 +0100

    removed E V E R Y T H I NG

--- lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/ws/FibonacciServiceIntf.java
+++
@@ -1,9 +0,0 @@
-package de.hska.iwi.vislab.lab1.example.ws;
-
-import javax.jws.*;
-@WebService
-public interface FibonacciServiceIntf {
-    //takes an integer n as its parameter
-    // and returns the nth number of the Fibonacci row as integer result
-    int getFibonacci(@WebParam(name = "n") int n);
-}
