e6188ace0ef8e1198292eacb20735ea202a6ec32
Daniel <danielkispert+github@googlemail.com>
2021-01-10 11:12:49 +0100

    oof

---
+++ lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/TestWsServer.java
@@ -0,0 +1,17 @@
+package de.hska.iwi.vislab.lab1.example;
+
+import javax.swing.JOptionPane;
+import javax.xml.ws.Endpoint;
+import de.hska.iwi.vislab.lab1.example.ws.FibonacciServiceImpl;
+
+
+/** Testserver fuer den Webservice */
+public class TestWsServer {
+	public static void main(final String[] args) {
+		String url = (args.length > 0) ? args[0]
+				: "http://localhost:4434/fibonacciservice";
+		Endpoint ep = Endpoint.publish(url, new FibonacciServiceImpl());
+		JOptionPane.showMessageDialog(null, "TestWsServer beenden");
+		ep.stop();
+	}
+}
