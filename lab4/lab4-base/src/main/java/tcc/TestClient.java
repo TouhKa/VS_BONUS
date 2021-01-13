a2ba6d2ab97a7d60e8664962de913b79a4360465
TouhKa <pia.s.schmidt@googlemail.com>
2021-01-13 17:45:23 +0100

    Add lab 4

--- lab4/lab4-base/src/main/java/tcc/TestClient.java
+++ lab4/lab4-base/src/main/java/tcc/TestClient.java
@@ -19,20 +19,22 @@ import tcc.hotel.HotelReservationDoc;
  */
 public class TestClient {
 	public static void main(String[] args) {
+		int counter = 0;
 		try {
 			Client client = ClientBuilder.newClient();
 			WebTarget target = client.target(TestServer.BASE_URI);
 
 			GregorianCalendar tomorrow = new GregorianCalendar();
 			tomorrow.setTime(new Date());
-			tomorrow.add(GregorianCalendar.DAY_OF_YEAR, 1);
+			// TODO change "amount" for rollback- szenario
+			tomorrow.add(GregorianCalendar.DAY_OF_YEAR, 0);
 
 			// book flight
 
 			WebTarget webTargetFlight = target.path("flight");
 
 			FlightReservationDoc docFlight = new FlightReservationDoc();
-			docFlight.setName("Christian");
+			docFlight.setName("Pia");
 			docFlight.setFrom("Karlsruhe");
 			docFlight.setTo("Berlin");
 			docFlight.setAirline("airberlin");
@@ -53,7 +55,7 @@ public class TestClient {
 			WebTarget webTargetHotel = target.path("hotel");
 
 			HotelReservationDoc docHotel = new HotelReservationDoc();
-			docHotel.setName("Christian");
+			docHotel.setName("Pia");
 			docHotel.setHotel("Interconti");
 			docHotel.setDate(tomorrow.getTimeInMillis());
 
@@ -67,6 +69,73 @@ public class TestClient {
 			HotelReservationDoc outputHotel = responseHotel.readEntity(HotelReservationDoc.class);
 			System.out.println("Output from Server: " + outputHotel);
 
+			//Confirm
+
+			Boolean everythingIsBooked = false;
+			String flightId = "";
+			String hotelId = "";
+
+			try {
+				//remove "/" in URI
+				flightId = outputFlight.getUrl().split(webTargetFlight.getUri().toString())[1].replace("/", "");
+				hotelId = outputHotel.getUrl().split(webTargetHotel.getUri().toString())[1].replace("/", "");
+				System.out.println(flightId + " " + hotelId);
+				System.out.println(outputFlight.getExpires() < tomorrow.getTimeInMillis());
+				System.out.println(outputHotel.getExpires() < tomorrow.getTimeInMillis());
+				System.out.println("Timestemp Send: " + tomorrow.getTimeInMillis());
+			} catch (NullPointerException e) {
+				System.out.println("No more hotels to book");
+				Response responseFlightCancel = webTargetFlight.path(flightId).request().delete();
+
+				if (responseFlightCancel.getStatus() != 200) {
+					System.out.println("Failed : HTTP error code : " + responseFlightCancel.getStatus());
+				}
+				return;
+			}
+
+			// As long as there are errors and whether the flight nor the hotel reservation
+			// have expired, try confirming
+			while (!everythingIsBooked && (outputFlight.getExpires() > tomorrow.getTimeInMillis())
+					&& (outputHotel.getExpires() > tomorrow.getTimeInMillis())) {
+				System.out.println("Not expired");
+				counter++;
+				Response responseFlightConfirm = webTargetFlight.path(flightId).request()
+						.put(Entity.text(MediaType.TEXT_PLAIN));
+
+				if (responseFlightConfirm.getStatus() != 200) {
+					System.out.println("Failed : HTTP error code : " + responseFlightConfirm.getStatus());
+					continue;
+				}
+
+				Response responseHotelConfirm = webTargetHotel.path(hotelId).request()
+						.put(Entity.text(MediaType.TEXT_PLAIN));
+				if (responseHotelConfirm.getStatus() != 200) {
+					System.out.println("Failed : HTTP error code : " + responseHotelConfirm.getStatus());
+					continue;
+				}
+
+				everythingIsBooked = true;
+			}
+
+			// Hotel or Flight reservation expired - so we cancel the remaining reservation
+			if (!everythingIsBooked) {
+				Response responseFlightCancel = webTargetFlight.path(flightId).request().delete();
+
+				if (responseFlightCancel.getStatus() != 200) {
+					System.out.println("Failed : HTTP error code : " + responseFlightCancel.getStatus());
+				}
+
+				Response responseHotelCancel = webTargetHotel.path(hotelId).request().delete();
+
+				if (responseHotelCancel.getStatus() != 200) {
+					System.out.println("Failed : HTTP error code : " + responseHotelCancel.getStatus());
+				}
+
+				System.out.println("Transactions rolled back! Nothing booked.");
+			} else {
+				System.out.println("OK! Flight and hotel are booked!");
+			}
+			System.out.println("Counter: " + counter);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
